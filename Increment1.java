458077ef52c96b597dfd50f53cc6ead73622c262
ArunDPrasad <arunajayasekara96@gmail.com>
2023-08-08 22:38:30 +0530

    Initial Commit

---
+++ Increment1.java
@@ -0,0 +1,26 @@
+public class Increment1{
+	public static void main(String args[]){
+		
+		// x++ : Post Increment
+		// ++x : Pre Increment
+		
+		int x = 5;
+		
+		System.out.println(x);//5
+		System.out.println(++x);//6
+		System.out.println(x);
+		
+		System.out.println();
+		
+		int y = 2;
+		System.out.println(y);//2
+		System.out.println(y++);//2
+		System.out.println(y);//3
+		
+
+		
+		
+		
+		
+	}
+}
